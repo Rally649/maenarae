6a228c745afaafae527e4c9bae5b3fa1e8a93dd0
Rally <elegantspring2@gmail.com>
2020-09-24 14:50:54 +0900

    必要最低限なもの以外を削除

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -16,20 +16,10 @@
 
 package com.example;
 
-import static javax.measure.unit.SI.*;
-
-import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Map;
 
-import javax.measure.quantity.Mass;
 import javax.sql.DataSource;
 
-import org.jscience.physics.amount.Amount;
-import org.jscience.physics.model.RelativisticModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
@@ -64,39 +54,6 @@ public class Main {
 	@RequestMapping("/")
 	String index() {
 		return "index";
-	}
-
-	@RequestMapping("/hello")
-	String hello(Map<String, Object> model) {
-		RelativisticModel.select();
-		String energy = System.getenv().get("ENERGY");
-		if (energy == null) {
-			energy = "12 GeV";
-		}
-		Amount<Mass> m = Amount.valueOf(energy).to(KILOGRAM);
-		model.put("science", "E=mc^2: " + energy + " = " + m.toString());
-		return "hello";
-	}
-
-	@RequestMapping("/db")
-	String db(Map<String, Object> model) {
-		try (Connection connection = dataSource.getConnection()) {
-			Statement stmt = connection.createStatement();
-			stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-			stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-			ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-
-			ArrayList<String> output = new ArrayList<String>();
-			while (rs.next()) {
-				output.add("Read from DB: " + rs.getTimestamp("tick"));
-			}
-
-			model.put("records", output);
-			return "db";
-		} catch (Exception e) {
-			model.put("message", e.getMessage());
-			return "error";
-		}
 	}
 
 	@Bean
